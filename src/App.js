aa095ff19f58fd3c64bfdb4ce4929c5f8de4dd5e
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-05-13 20:52:02 -0700

    Created the recipe component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-import axios from "axios";
+import Recipe from "./Recipe";
 import "./App.css";
 
 function App() {
-  const apiId = "e6f467f5";
-  const apiKey = "ba93ffc5731ad174b73b38733e12f25c";
-  const apiUrl = `https://api.edamam.com/search?q=chicken&app_id=${apiId}&app_key=${apiKey}`;
-
   return (
     <div className="App">
-      <h1>Hello Karyn</h1>
+      <Recipe />
     </div>
   );
 }
