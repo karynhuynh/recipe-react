fc608f0e601b583c73fff05bf64b2b1052b828b2
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-05-15 14:35:38 -0700

    Set a default keyword on load

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Recipe() {
+export default function Recipe(props) {
   useEffect(() => {}, []);
 
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
 
   function foodSubmit(event) {
     event.preventDefault();
     search();
+  }
+
+  function foodSearch(event) {
+    setKeyword(event.target.value);
   }
 
   function load() {
@@ -22,7 +27,7 @@ export default function Recipe() {
     //   documentation: https://developer.edamam.com/edamam-docs-recipe-api
     const apiId = "e6f467f5";
     const apiKey = "ba93ffc5731ad174b73b38733e12f25c";
-    let apiUrl = `https://api.edamam.com/search?q=chicken&app_id=${apiId}&app_key=${apiKey}`;
+    let apiUrl = `https://api.edamam.com/search?q=${keyword}&app_id=${apiId}&app_key=${apiKey}`;
     axios.get(apiUrl).then(handleRecipeResponse);
   }
 
@@ -35,7 +40,14 @@ export default function Recipe() {
     return (
       <div className="App">
         <form className="search-form" onSubmit={foodSubmit}>
-          <input className="search-bar" type="text" autoFocus={true} required />
+          <input
+            className="search-bar"
+            type="text"
+            defaultValue={props.defaultKeyword}
+            autoFocus={true}
+            required
+            onChange={foodSearch}
+          />
           <button className="search-button" type="submit">
             Search
           </button>
