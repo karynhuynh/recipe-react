f046adb42d524e043b1d8fe9c11139a34f8f6157
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-05-15 14:36:05 -0700

    Removed the link from the image and created a source prop

--- src/RecipeImage.js
+++ src/RecipeImage.js
@@ -4,9 +4,8 @@ export default function RecipeImage(props) {
   if (props.image) {
     return (
       <div className="RecipeImage">
-        <a href={props.image.url} target="_blank" rel="noreferrer">
+        {/* <a href={props.image.url} target="_blank" rel="noreferrer"></a> */}
           <img src={props.image.image} alt="recipe final result" />
-        </a>
       </div>
     );
   } else {
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Ingredients from "./Ingredients";
+// import Ingredients from "./Ingredients";
 import RecipeImage from "./RecipeImage";
+import RecipeInfo from './RecipeInfo.js'
 
 export default function Results(props) {
   if (props.results) {
@@ -13,8 +14,17 @@ export default function Results(props) {
               <div className="RecipeImage">
                 <RecipeImage image={result.recipe} />
               </div>
-              <div className="Ingredients">
+              {/* <div className="Ingredients">
                 <Ingredients ingredients={result.recipe.ingredientLines} />
+              </div> */}
+              <div className="RecipeInfo">
+                <RecipeInfo info={result.recipe.dishType}/>
+              </div>
+              <div className="RecipeSource">
+                <span>Source: </span>
+                <a href={result.recipe.url} target="_blank" rel="noreferrer">
+                  {result.recipe.source}
+                </a>
               </div>
             </div>
           );
