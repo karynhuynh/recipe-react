df8203991548a157b0660137926e014440f520f7
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-05-19 20:25:10 -0700

    Condensed components

--- src/RecipeInfo.js
+++ src/RecipeInfo.js
@@ -6,11 +6,21 @@ export default function RecipeInfo(props) {
   if (props.info) {
     return (
       <div className="RecipeInfo">
-        <div className="Serving">
-          Serves <span className="amount">{props.info.yield}</span>
+        <div className="numbers">
+          <div className="serving">
+            Serves <span className="amount">{props.info.yield}</span>
+          </div>
+          <div className="calories">
+            <span className="amount">{Math.round(props.info.calories)}</span>{" "}
+            kCal
+          </div>
         </div>
-        <div className="Calories">
-          <span className="amount">{Math.round(props.info.calories)}</span> kCal
+        <div className="category">
+          Category: <span className="category-info">{props.info.dishType}</span>
+        </div>
+        <div className="source">
+          <span>Source: </span>
+          {props.info.source}
         </div>
       </div>
     );
--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,6 @@
 import React from "react";
 // import Ingredients from "./Ingredients";
 import RecipeImage from "./RecipeImage";
-import Category from "./Category.js";
 import RecipeInfo from "./RecipeInfo";
 
 import "./Results.css";
@@ -13,20 +12,15 @@ export default function Results(props) {
         {props.results.map((result, index) => {
           return (
             <div key={index} className="results-card">
-              <h3>{result.recipe.label}</h3>
-              <RecipeInfo info={result.recipe} />
               <RecipeImage
                 image={result.recipe.image}
                 url={result.recipe.url}
               />
+              <h3>{result.recipe.label}</h3>
+              <RecipeInfo info={result.recipe} />
               {/* <div className="Ingredients">
                 <Ingredients ingredients={result.recipe.ingredientLines} />
               </div> */}
-              <Category category={result.recipe.dishType} />
-              <div className="RecipeSource">
-                <span>Source: </span>
-                {result.recipe.source}
-              </div>
             </div>
           );
         })}
